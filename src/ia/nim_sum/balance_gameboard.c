3b959bab5454aa8a3a24a738f8d8327263294038
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-23 17:06:37 +0100

    Fixed AI infinite boucle once again

--- src/ia/nim_sum/balance_gameboard.c
+++ src/ia/nim_sum/balance_gameboard.c
@@ -20,41 +20,54 @@ static int choose_line_to_delete(int const nb_sticks[], int largest_u)
     return (no_line);
 }
 
-static int get_default_matches(int balance, int largest_u)
+static int get_default_matches(int value, int largest_u)
 {
     int mask = 1;
     int matches = 0;
 
-    while (balance != largest_u) {
-        if (balance & mask) {
-            balance ^= mask;
+    while ((largest_u & mask) == 0) {
+        if (value & mask)
             matches += mask;
-        } else
-            mask = mask << 1;
-        my_printf("Balance: %.4b\nMask: %.4b\n", balance, mask);
+        mask = mask << 1;
+        my_printf("New value: %.4b\nMask: %.4b\n", value, mask);
     }
     return (matches);
+}
+
+static void next_matchstick_line(int const nb_sticks[], int *line, int l_u)
+{
+    do {
+        *line += 1;
+        my_printf("Line %d in binary: %.4b\n", *line, nb_sticks[*line]);
+        my_printf("Largest unbalanced: %.4b\n", l_u);
+        my_printf("Result with mask: %.4b\n", (nb_sticks[*line] & l_u));
+    } while ((nb_sticks[*line] & l_u) == 0);
 }
 
 static int get_matches(int nb_sticks[], int *line, int largest_u)
 {
     int balance = get_balance(nb_sticks);
-    int default_matches = get_default_matches(balance, largest_u);
+    int default_matches = get_default_matches(nb_sticks[*line], largest_u);
     int matches = default_matches;
     int save_nb_sticks = nb_sticks[*line];
 
     nb_sticks[*line] -= matches;
-    while (get_balance(nb_sticks) != 0) {
+    while ((balance = get_balance(nb_sticks)) != 0) {
+        my_printf("Balance: %.4b\n", balance);
         nb_sticks[*line] -= 1;
         matches += 1;
         if (nb_sticks[*line] == 0) {
             nb_sticks[*line] = save_nb_sticks;
-            *line += 1;
+            next_matchstick_line(nb_sticks, line, largest_u);
             save_nb_sticks = nb_sticks[*line];
             matches = default_matches;
             nb_sticks[*line] -= matches;
+            my_printf("Line: %d (removed %d matches)\n", nb_sticks[*line], matches);
         }
+        if (nb_sticks[*line] < 0)
+            return (-1);
     }
+    my_printf("Final Balance: %.4b\n", balance);
     return (matches);
 }
 
@@ -67,8 +80,9 @@ int balance_gameboard(input_t *input, int nb_sticks[], int max_matches)
 
     if (line < 0)
         return (0);
+    my_printf("Line chosen: %d\n", line);
     matches = get_matches(nb_sticks, &line, largest_unbalanced);
-    if (matches > max_matches)
+    if (matches <= 0 || matches > max_matches)
         return (0);
     input->line = line;
     input->matches = matches;
