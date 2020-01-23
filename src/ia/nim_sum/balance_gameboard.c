634ce19f5aa80f3f190a0fe414ad8e4ed9fe1ae4
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-23 15:49:58 +0100

    Fixed an another infinte boucle

--- src/ia/nim_sum/balance_gameboard.c
+++ src/ia/nim_sum/balance_gameboard.c
@@ -20,9 +20,8 @@ static int choose_line_to_delete(int const nb_sticks[], int largest_u)
     return (no_line);
 }
 
-static int get_matches(int nb_sticks[], int line, int largest_u)
+static int get_default_matches(int balance, int largest_u)
 {
-    int balance = get_balance(nb_sticks);
     int mask = 1;
     int matches = 0;
 
@@ -32,11 +31,29 @@ static int get_matches(int nb_sticks[], int line, int largest_u)
             matches += mask;
         } else
             mask = mask << 1;
+        my_printf("Balance: %.4b\nMask: %.4b\n", balance, mask);
     }
-    nb_sticks[line] -= matches;
+    return (matches);
+}
+
+static int get_matches(int nb_sticks[], int *line, int largest_u)
+{
+    int balance = get_balance(nb_sticks);
+    int default_matches = get_default_matches(balance, largest_u);
+    int matches = default_matches;
+    int save_nb_sticks = nb_sticks[*line];
+    
+    nb_sticks[*line] -= matches;
     while (get_balance(nb_sticks) != 0) {
-        nb_sticks[line] -= 1;
+        nb_sticks[*line] -= 1;
         matches += 1;
+        if (nb_sticks[*line] == 0) {
+            nb_sticks[*line] = save_nb_sticks;
+            *line += 1;
+            save_nb_sticks = nb_sticks[*line];
+            matches = default_matches;
+            nb_sticks[*line] -= matches;
+        }
     }
     return (matches);
 }
@@ -50,7 +67,7 @@ int balance_gameboard(input_t *input, int nb_sticks[], int max_matches)
 
     if (line < 0)
         return (0);
-    matches = get_matches(nb_sticks, line, largest_unbalanced);
+    matches = get_matches(nb_sticks, &line, largest_unbalanced);
     if (matches > max_matches)
         return (0);
     input->line = line;
